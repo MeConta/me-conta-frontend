d2bcb8f71ccdf5ed99555e1c289a4bb0c9d92bb8
guilhermegiarola <guilherme.giarola@thoughtworks.com>
2021-09-28 17:37:31 -0300

    feat(#22): implementação do serviço de cadastro

--- src/services/signup-service/signup-service.tsx
+++ src/services/signup-service/signup-service.tsx
@@ -1,4 +1,5 @@
 import { createContext, PropsWithChildren } from 'react'
+import axios from 'axios'
 
 export type SignupUser = {
   email: string
@@ -10,8 +11,8 @@ export interface ISignupService {
 }
 
 export class SignupService implements ISignupService {
-  initialSignup(user: SignupUser) {
-    console.log(user)
+  async initialSignup(user: SignupUser) {
+    await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/user` || '', user)
   }
 }
 
