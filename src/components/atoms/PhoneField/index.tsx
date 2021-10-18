043a7fae5701849c4031ca3e7115b1f93deda91e
Luiz Felipe <luizfelipe.barbosa@thoughtworks.com>
2021-10-18 14:58:42 -0300

    feat: adicionando mascara dinamica ao telefone

--- src/components/atoms/PhoneField/index.tsx
+++ src/components/atoms/PhoneField/index.tsx
@@ -33,11 +33,19 @@ export function PhoneField({
     }
   }
 
+  const maskBuilder = (value: string | undefined): string => {
+    if (!value || value.length == 0) return ''
+    const clearValue = removePhoneMask(value)
+    return clearValue.length >= 10 && clearValue[2] === '9'
+      ? '(99) 99999-9999'
+      : '(99) 9999-9999'
+  }
+
   return (
     <FormGroup label={label} name={name} error={error}>
       <S.InputWrapper>
         <InputMask
-          mask={'(99) 99999-9999'}
+          mask={maskBuilder(value)}
           onBlur={props.onBlur}
           onChange={handleChange}
           value={value}
--- src/components/atoms/PhoneField/test.tsx
+++ src/components/atoms/PhoneField/test.tsx
@@ -13,22 +13,24 @@ describe('<PhoneField/>', () => {
   it('Should be defined', () => {
     expect(true).toBeTruthy()
   })
-  // it('Should show phone mask in input value', () => {
-  //   const inputNumber = '99999999999'
-  //
-  //   render(<PhoneField data-testid="phone-number" name="phone" label="phone" />)
-  //   const inputElement = screen.getByTestId('phone-number')
-  //   fireEvent.change(inputElement, { target: { value: inputNumber } })
-  //   expect(inputElement).toHaveDisplayValue('(99) 99999-9999')
-  // })
-  //
-  // it('should not change value with text input', async () => {
-  //   render(<PhoneField data-testid="phone-number" name="phone" label="phone" />)
-  //   const inputElement = screen.getByTestId('phone-number')
-  //   const text = 'abcdef'
-  //   userEvent.type(inputElement, text)
-  //   await waitFor(() => {
-  //     expect(inputElement).toHaveValue('')
-  //   })
-  // })
+  it('Should show phone mask in input value', () => {
+    const inputNumber = '99999999999'
+
+    render(<PhoneField data-testid="phone-number" name="phone" label="phone" />)
+    const inputElement = screen.getByTestId('phone-number')
+    fireEvent.change(inputElement, { target: { value: inputNumber } })
+    waitFor(() => {
+      expect(inputElement).toHaveDisplayValue('(99) 99999-9999')
+    })
+  })
+
+  it('should not change value with text input', async () => {
+    render(<PhoneField data-testid="phone-number" name="phone" label="phone" />)
+    const inputElement = screen.getByTestId('phone-number')
+    const text = 'abcdef'
+    userEvent.type(inputElement, text)
+    await waitFor(() => {
+      expect(inputElement).toHaveValue('')
+    })
+  })
 })
