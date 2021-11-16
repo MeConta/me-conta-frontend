4905c75c7eef3e5344b27612bb25dbf6b71bdc8c
Diego Lopes <diego.lopes@thoughtworks.com>
2021-11-16 18:41:16 -0300

    fix: alterações no docker para rodar o compose do front

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-  reactStrictMode: true,
+  /*reactStrictMode: true,
   env: {
     NEXT_PUBLIC_API_URL: process.env.API_URL
+  }*/
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.API_URL || 'http://localhost:3000'}/:path*`
+      }
+    ]
   }
 }
-console.log(process.env.API_URL)
+// console.log(process.env.API_URL)
--- src/services/api/api.tsx
+++ src/services/api/api.tsx
@@ -14,5 +14,6 @@ api.interceptors.request.use((config) => {
     const token = JSON.parse(tokenLocalStorage)
     config.headers.Authorization = `Bearer ${token}`
   }
+  config.url?.concat('api/', config.url)
   return config
 })
