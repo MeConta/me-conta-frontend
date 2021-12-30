5cc32c07a1105fabe6def96682fc9fa4cf74250d
Augusto Dias <augustodg2@gmail.com>
2021-12-30 16:29:31 -0300

    refactor: remove unused control

--- src/components/molecules/DateSelect/stories.tsx
+++ src/components/molecules/DateSelect/stories.tsx
@@ -1,15 +1,20 @@
 import { Story, Meta } from '@storybook/react'
 
 import { DateSelect, DateSelectProps } from '.'
+import { DateInfo } from './dateInfo'
 
 export default {
   component: DateSelect,
   title: 'Molecules/DateSelect',
   argTypes: {
-    onChange: { action: 'changed', control: false }
+    onChange: { action: 'changed' }
   }
 } as Meta
 
 export const Default: Story<DateSelectProps> = (args) => (
   <DateSelect {...args} />
 )
+
+Default.args = {
+  availabilty: DateInfo
+}
