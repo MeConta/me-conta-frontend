ad3bc2f0d0939e12535917b60450149e43804f4b
Augusto Dias <augustodg2@gmail.com>
2021-12-31 08:36:53 -0300

    feat: adiciona mais datas no DateInfo

--- src/components/molecules/DateSelect/dateInfo.tsx
+++ src/components/molecules/DateSelect/dateInfo.tsx
@@ -12,5 +12,33 @@ export const DateInfo = [
     day: new Date(2021, 10, 26),
     times: [new Date(2021, 10, 26, 14), new Date(2021, 10, 26, 17)]
   },
+  { day: new Date(2021, 10, 26), times: [] },
+  {
+    day: new Date(2021, 10, 24),
+    times: [
+      new Date(2021, 10, 24, 13),
+      new Date(2021, 10, 24, 14),
+      new Date(2021, 10, 24, 18)
+    ]
+  },
+  { day: new Date(2021, 10, 25), times: [new Date(2021, 10, 25, 15)] },
+  {
+    day: new Date(2021, 10, 26),
+    times: [new Date(2021, 10, 26, 14), new Date(2021, 10, 26, 17)]
+  },
+  { day: new Date(2021, 10, 26), times: [] },
+  {
+    day: new Date(2021, 10, 24),
+    times: [
+      new Date(2021, 10, 24, 13),
+      new Date(2021, 10, 24, 14),
+      new Date(2021, 10, 24, 18)
+    ]
+  },
+  { day: new Date(2021, 10, 25), times: [new Date(2021, 10, 25, 15)] },
+  {
+    day: new Date(2021, 10, 26),
+    times: [new Date(2021, 10, 26, 14), new Date(2021, 10, 26, 17)]
+  },
   { day: new Date(2021, 10, 26), times: [] }
 ]
