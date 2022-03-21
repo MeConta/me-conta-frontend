bb2a47e0c1d0bc2b4a48eef6826ac5a5add2bce9
Carla San Roman <carla.s@thoughtworks.com>
2022-03-21 17:21:22 +0100

    test: add AuthService class unit tests

---
+++ src/services/auth-services/auth-service.spec.ts
@@ -0,0 +1,61 @@
+import { AuthService } from './auth-service'
+
+import * as axios from 'axios'
+import { UserType } from '../../enums/user-type.enum'
+
+describe('AuthService', () => {
+  let service: AuthService
+  jest.mock('axios')
+  beforeEach(() => {
+    service = new AuthService(axios.default)
+  })
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('Deve instanciar o serviço', () => {
+    expect(service).toBeDefined()
+  })
+
+  it('Must do a post request to api/auth/login/', async () => {
+    jest.spyOn(axios.default, 'post').mockResolvedValue({
+      data: {
+        token: 'mocked',
+        tipo: UserType.ATENDENTE,
+        nome: 'Jack Tester'
+      }
+    })
+    await service.login({
+      email: 'teste@teste.com',
+      senha: 's3nh$F0R!3'
+    })
+    expect(axios.default.post).toBeCalled()
+    expect(axios.default.post).toReturn()
+    expect(axios.default.post).toBeCalledWith('api/auth/login/', {
+      password: 's3nh$F0R!3',
+      username: 'teste@teste.com'
+    })
+  })
+
+  it('Must do a post request to /senha/recuperacao/', async () => {
+    jest.spyOn(axios.default, 'post')
+    await service.recuperarSenha('teste@teste.com')
+    expect(axios.default.post).toBeCalled()
+    expect(axios.default.post).toBeCalledWith('/senha/recuperacao/', {
+      email: 'teste@teste.com'
+    })
+  })
+
+  it('Must do a post request to /senha/reset/', async () => {
+    jest.spyOn(axios.default, 'post')
+    await service.resetarSenha({
+      hash: 'hash',
+      senha: 's3nh$F0R!3'
+    })
+    expect(axios.default.post).toBeCalled()
+    expect(axios.default.post).toBeCalledWith('/senha/reset/', {
+      senha: 's3nh$F0R!3',
+      hash: 'hash'
+    })
+  })
+})
