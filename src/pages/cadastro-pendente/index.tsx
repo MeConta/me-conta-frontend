068a1fb54bb8bd0223fd7bf0d4f0d34196a00186
Wilder Carvalho <wilder.carvalho@thoughtworks.com>
2022-05-12 08:52:33 -0300

    feat(#57): limitação de acesso à rota cadastro-pendente

--- src/pages/cadastro-pendente/index.tsx
+++ src/pages/cadastro-pendente/index.tsx
@@ -1,15 +1,35 @@
 import ConfirmationDialog from 'components/molecules/ConfirmationDialog'
+import { UserType } from 'enums/user-type.enum'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { useAuthContext } from 'store/auth-context'
+import { redirects } from 'utils/routes/redirects'
 
 export default function CadastroPendente() {
+  const router = useRouter()
+  const authCtx = useAuthContext()
+  const [renderPendente, setRenderPendente] = useState<boolean>(false)
+  useEffect(() => {
+    if (!authCtx.isLoggedIn) {
+      router.push('/login')
+    } else if (+authCtx.session.type !== UserType.ATENDENTE) {
+      router.push(redirects[+authCtx.session.type])
+    } else {
+      setRenderPendente(true)
+    }
+  }, [authCtx.isLoggedIn, authCtx.session.type, router])
+
   return (
-    <ConfirmationDialog
-      titleInfo={{ preText: 'SUA INSCRIÇÃO FOI ', boldText: 'CONCLUÍDA!' }}
-      subtitleInfo={{
-        preText: 'Nossa equipe irá analisar seu perfil e entrará em ',
-        boldText: 'contato por e-mail ',
-        posText: 'em breve.'
-      }}
-      buttonText="VOLTAR À PÁGINA INICIAL"
-    />
+    renderPendente && (
+      <ConfirmationDialog
+        titleInfo={{ preText: 'SUA INSCRIÇÃO FOI ', boldText: 'CONCLUÍDA!' }}
+        subtitleInfo={{
+          preText: 'Nossa equipe irá analisar seu perfil e entrará em ',
+          boldText: 'contato por e-mail ',
+          posText: 'em breve.'
+        }}
+        buttonText="VOLTAR À PÁGINA INICIAL"
+      />
+    )
   )
 }
