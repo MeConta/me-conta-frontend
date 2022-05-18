b2dc281b640cd39c3d2705b8076d025b93392988
Wilder Carvalho <wilder.carvalho@thoughtworks.com>
2022-05-18 12:33:00 -0300

    feat(#103): creation of animation to modal

--- src/components/molecules/ConfirmationDialog/styles.tsx
+++ src/components/molecules/ConfirmationDialog/styles.tsx
@@ -1,8 +1,18 @@
-import styled, { css, DefaultTheme } from 'styled-components'
+import styled, { css, DefaultTheme, keyframes } from 'styled-components'
 
 type ContainerProps = {
   isModal?: boolean
 }
+
+const fadeIn = keyframes`
+  0% {opacity: 0}
+  100% {opacity: 1}
+`
+
+const fadeInFromTop = keyframes`
+  0% {opacity: 0; top: -50px}
+  100% {opacity: 1; top: 0}
+`
 
 const modalStyles = (theme: DefaultTheme) => css`
   background-color: rgba(0, 0, 0, 0.25);
@@ -12,6 +22,13 @@ const modalStyles = (theme: DefaultTheme) => css`
   height: 100%;
 `
 
+const modalAnimation = () => css`
+  position: relative;
+  opacity: 0;
+  animation: ${fadeInFromTop} 0.5s cubic-bezier(0.22, 0.61, 0.36, 1) forwards;
+  animation-delay: 0.35s;
+`
+
 export const DivContainer = styled.div<ContainerProps>`
   ${({ isModal, theme }) => css`
     width: 100%;
@@ -19,6 +36,11 @@ export const DivContainer = styled.div<ContainerProps>`
     justify-content: center;
     align-items: center;
     backdrop-filter: ${isModal && 'blur(5px)'};
+    animation: ${isModal && fadeIn} 0.5s;
+
+    & > div {
+      ${isModal && modalAnimation()}
+    }
 
     ${!!isModal && modalStyles(theme)}
 
