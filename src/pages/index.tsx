39282e56625bfcc4ea9e096dc2d7dafbaa0cad98
Manuella Vieira <manuella.vieira@thoughtworks.com>
2022-05-30 18:22:12 -0300

    fix: remove ConfirmationDialog from index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,3 @@
-import ConfirmationDialog from 'components/molecules/ConfirmationDialog'
-
 export default function Home() {
-  return (
-    <ConfirmationDialog
-      titleInfo={{ preText: 'Test' }}
-      buttonText="Test"
-      isClosable
-      isModal
-    ></ConfirmationDialog>
-  )
+  return <h1>Index</h1>
 }
