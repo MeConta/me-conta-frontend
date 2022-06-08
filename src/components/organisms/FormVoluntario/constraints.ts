6ce8f9959aff44f10f5f3d827989661aa9a445fb
Manuella Vieira <manuella.vieira@thoughtworks.com>
2022-06-08 10:56:27 -0300

    chore: removes old unused registration pages and forms

--- src/components/organisms/FormVoluntario/constraints.ts
+++
@@ -1,6 +0,0 @@
-export const MAX_LENGTH_CITY_VALUE = 100
-export const MIN_LENGTH_CITY_VALUE = 3
-export const MAX_LENGTH_NAME_VALUE = 100
-export const MIN_LENGTH_NAME_VALUE = 2
-export const LENGTH_PHONE_VALUE = 11
-export const MIN_LENGTH_PHONE_VALUE = 10
--- src/components/organisms/FormVoluntario/situacao-curso.ts
+++
@@ -1,6 +0,0 @@
-enum ESituacaoCurso {
-  COMPLETO = `COMPLETO`,
-  ANDAMENTO = `ANDAMENTO`
-}
-
-export default ESituacaoCurso
