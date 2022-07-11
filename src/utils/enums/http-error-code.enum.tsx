1b35766ae4b5b6883282361c4e14fae224790b8f
Giovanna Batalha <giovanna.batalha@thoughtworks.com>
2022-07-11 15:10:31 -0300

    feat(#118): adjustment in toast error message, dealing with error message as array

--- src/utils/enums/http-error-code.enum.tsx
+++ src/utils/enums/http-error-code.enum.tsx
@@ -1,10 +1,13 @@
 import { AxiosError } from 'axios'
+import { array } from 'yup'
 
 export function getErrorMessage(error: AxiosError) {
   const errorMessage = error?.response?.data?.message
 
   if (errorMessage) {
-    return errorMessage
+    return Array.isArray(errorMessage) && errorMessage.length > 0
+      ? errorMessage[0]
+      : errorMessage
   }
   return (
     <div>
