977868c8448975f806a382f8332d07f8f4b6b87e
Vanessa Furtado de Lima <vanessalima@MacBook-Pro-de-Vanessa.local>
2022-09-14 13:17:53 -0300

    fix(#183): creating scheduled session card and tests

--- src/components/molecules/CardScheduledSession/stories.tsx
+++ src/components/molecules/CardScheduledSession/stories.tsx
@@ -1,13 +1,9 @@
 import { Story, Meta } from '@storybook/react'
 import { CardScheduledSession, CardScheduledSessionProps } from '.'
-import { DateInfo } from '../DateSelect/dateInfo'
 
 export default {
   component: CardScheduledSession,
-  title: 'Molecules/CardScheduledSession',
-  argTypes: {
-    onChange: { action: 'changed' }
-  }
+  title: 'Molecules/CardScheduledSession'
 } as Meta
 
 export const Default: Story<CardScheduledSessionProps> = (args) => (
@@ -17,5 +13,5 @@ export const Default: Story<CardScheduledSessionProps> = (args) => (
 Default.args = {
   name: 'Luvois Zaladar',
   email: 'jitewaboh@lagify.com',
-  frentes: [0, 1, 2],
+  frentes: [0, 1, 2]
 }
